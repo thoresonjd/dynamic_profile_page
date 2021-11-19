6ea6e41f044ab2f606dc4c4c35c7d774e237ffe0
Justin Thoreson <thoresonjd@gmail.com>
2021-11-19 15:22:24 -0800

    Debug show checked mode banner false

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class DynamicProfilePageApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: const ProfileSetupPage()
     );
