e98139bbdbc78c4129cf96b4d3480313b9bf7b6a
Justin Thoreson <thoresonjd@gmail.com>
2021-11-19 13:53:54 -0800

    Added profile field types

--- lib/db/model/profile.dart
+++ lib/db/model/profile.dart
@@ -1,5 +1,11 @@
 final String profileTable = 'profile';
 
+class ProfileTypes {
+  static final String id = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+  static final String username = 'VARCHAR(30) NOT NULL';
+  static final String description = 'VARCHAR(250)';
+}
+
 class ProfileFields {
   static final String id = '_id';
   static final String username = 'username';
