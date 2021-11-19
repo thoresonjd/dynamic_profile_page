181895f322e810f997581daaf1dd1d56cd2b18ca
Justin Thoreson <thoresonjd@gmail.com>
2021-11-19 11:18:37 -0800

    Set up main file w/ theme and main app widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dynamic_profile_page/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const DynamicProfilePageApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
