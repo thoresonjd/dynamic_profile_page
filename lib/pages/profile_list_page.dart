cf937f09d7aa6c385b8aa0606ea921a54c13266c
Justin Thoreson <thoresonjd@gmail.com>
2021-11-19 15:21:38 -0800

    Added listview builder to profile list page

--- lib/pages/profile_list_page.dart
+++ lib/pages/profile_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_profile_page/db/model/profile.dart';
+import 'package:dynamic_profile_page/db/profile_database.dart';
 
 class ProfileListPage extends StatefulWidget {
   const ProfileListPage({ Key? key }) : super(key: key);
@@ -8,10 +10,50 @@ class ProfileListPage extends StatefulWidget {
 }
 
 class _ProfileListPageState extends State<ProfileListPage> {
+  late List<Profile> profiles;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    refreshProfiles();
+  }
+
+  @override dispose() {
+    ProfileDatabase.instance.close();
+    super.dispose();
+  }
+
+  Future refreshProfiles() async {
+    setState(() => isLoading = true);
+    profiles = await ProfileDatabase.instance.readAllProfiles();
+    setState(() => isLoading = false);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profiles"),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: profiles.length,
+        itemBuilder: (context, index) {
+          final profile = profiles[index];
+          
+          return GestureDetector(
+            onTap: () async {
+              refreshProfiles();
+            },
+
+            child: ListTile(
+              leading: Text('${profile.id}'),
+              title: Text(profile.username)
+            )
+          );
+        }
+      )
     );
   }
 }
