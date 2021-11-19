d1eae89b47862d4c580cbe43bf585880080dfdb2
Justin Thoreson <thoresonjd@gmail.com>
2021-11-19 14:54:49 -0800

    Created profile list page

---
+++ lib/pages/profile_list_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileListPage extends StatefulWidget {
+  const ProfileListPage({ Key? key }) : super(key: key);
+
+  @override
+  _ProfileListPageState createState() => _ProfileListPageState();
+}
+
+class _ProfileListPageState extends State<ProfileListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
