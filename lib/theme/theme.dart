4d6499d22a41e0d6fbc277f00fc8378d31801ad3
Justin Thoreson <thoresonjd@gmail.com>
2021-11-21 00:11:11 -0800

    Created custom theme data

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,5 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:dynamic_profile_page/theme/colors.dart';
 
-ThemeData AppTheme() {
+ThemeData appTheme() {
   final ThemeData base = ThemeData.dark();
+
+  AppBarTheme _appBarTheme(AppBarTheme base) {
+    return base.copyWith(
+      color: AppColors.invis,
+      centerTitle: true,
+      elevation: 0,
+      titleTextStyle: const TextStyle(
+        color: AppColors.white,
+        fontWeight: FontWeight.bold
+      )
+    );
+  }
+
+  ButtonThemeData _buttonTheme(ButtonThemeData base) {
+    return base.copyWith(
+      buttonColor: AppColors.lime,
+    );
+  }
+
+  return base.copyWith(
+    appBarTheme: _appBarTheme(base.appBarTheme),
+    buttonTheme: _buttonTheme(base.buttonTheme)
+  );
 }
