ab050a070111111f5b2efc64e5d4d9e70599bd65
Justin Thoreson <thoresonjd@gmail.com>
2021-11-21 10:30:47 -0800

    Replaced page view with scaffold with bottom nav bar

--- lib/pages/landing_page.dart
+++ lib/pages/landing_page.dart
@@ -2,6 +2,7 @@ import 'package:dynamic_profile_page/db/profile_database.dart';
 import 'package:dynamic_profile_page/pages/home_page.dart';
 import 'package:dynamic_profile_page/pages/profile_list_page.dart';
 import 'package:flutter/material.dart';
+import 'package:dynamic_profile_page/theme/colors.dart';
 
 class LandingPage extends StatefulWidget {
   const LandingPage({ Key? key }) : super(key: key);
@@ -19,14 +20,45 @@ class _LandingPageState extends State<LandingPage> {
 
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController(initialPage: 1);
-    return PageView(
-      scrollDirection: Axis.horizontal,
-      controller: controller,
-      children: const <Widget>[
-        HomePage(),
-        ProfilesPage(),
-      ],
+    return Scaffold(
+      bottomNavigationBar: Container(                                             
+        decoration: const BoxDecoration(                                                   
+          borderRadius: BorderRadius.only(                                           
+            topRight: Radius.circular(30), 
+            topLeft: Radius.circular(30)
+          ),            
+          boxShadow: [                                                               
+            BoxShadow(
+              color: AppColors.black, 
+              spreadRadius: 0, 
+              blurRadius: 10
+            ),       
+          ],                                                                         
+        ),                                                                           
+        child: ClipRRect(                                                            
+          borderRadius: const BorderRadius.only(                                           
+          topLeft: Radius.circular(30.0),                                            
+          topRight: Radius.circular(30.0),                                           
+          ),                                                                         
+          child: BottomNavigationBar(                                                
+            items: const <BottomNavigationBarItem>[                                        
+              BottomNavigationBarItem(                                               
+                icon: Icon(Icons.home), label: 'Home'),               
+              BottomNavigationBarItem(                                               
+                icon: Icon(Icons.list), label: 'Profiles')                
+            ],                                                                       
+          ),                                                                         
+        )                                                                            
+      )
     );
+    // final PageController controller = PageController(initialPage: 1);
+    // return PageView(
+    //   scrollDirection: Axis.horizontal,
+    //   controller: controller,
+    //   children: const <Widget>[
+    //     HomePage(),
+    //     ProfilesPage(),
+    //   ],
+    // );
   }
 }
