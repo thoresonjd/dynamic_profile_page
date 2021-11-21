2f60a0551c9859495d1fcaf8c063e62b3407d35c
Justin Thoreson <thoresonjd@gmail.com>
2021-11-20 23:38:14 -0800

    Created custom colors

---
+++ lib/theme/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const red = Color(0xffff0000);
+  static const orange = Color(0xffff7900);
+  static const yellow = Color(0xffffff00);
+  static const lime = Color(0xff00ff00);
+  static const cyan = Color(0xff00ffff);
+  static const magenta = Color(0xffff00ff);
+  static const white = Color(0xffffffff);
+  static const grey = Color(0xff808080);
+  static const black = Color(0xff000000);
+  static const invis = Colors.transparent;
+}
